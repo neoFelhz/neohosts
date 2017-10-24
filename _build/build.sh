c253f115b584f1d59b14a98c22bcf3f702fc5bfd
neoFelhz <neoFelhz@gmail.com>
2017-10-24 13:23:39 +0800

    chore(ci): bring up automatic build using Travis CI (#6)

--- _build/build.sh
+++ _build/build.sh
@@ -1,3 +1,5 @@
-find ./_data -type f -name "*.txt" -print0 | xargs --null cat > all.txt
-sort all.txt -u > hosts
-rm all.txt
+find ./_data -type f -name "*.txt" | xargs cat > ./_build/tmp/all.txt
+sort ./_build/tmp/all.txt -u > ./_build/tmp/hosts-standard.txt
+cat ./_build/head.txt ./_build/tmp/hosts-standard.txt > ./_build/tmp/hosts
+rm ./_build/tmp/all.txt
+rm ./_build/tmp/hosts-standard.txt
--- _build/deploy.sh
+++ _build/deploy.sh
@@ -3,10 +3,10 @@ git init
 git config --global push.default matching
 git config --global user.email "${GitHubEMail}"
 git config --global user.name "${GitHubUser}"
-git remote add origin https://${GitHubKEY}@github.com/${GitHubUser}/neohosts.git
+git remote add origin https://${GitHubKEY}@github.com/neko-dev/neohosts.git
 git pull origin gh-pages
 rm -rf ./*
-cp -rf ../_build/hosts ./
+cp -rf ../_build/tmp/hosts ../_public/
 git add --all .
-git commit -m "neoHosts Nightly Build by Travis CI"
+git commit -m "neoHosts Automatic Build by Travis CI"
 git push --quiet --force origin HEAD:gh-pages
