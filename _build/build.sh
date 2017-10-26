fe1dc1596fe4e13e0c4321a38c53ba278510658b
neoFelhz <neofelhz@gmail.com>
2017-10-26 12:48:58 +0800

    chore/data: minor changes - add remove comment support for build.sh - update coinhave domain - add more about safety

--- _build/build.sh
+++ _build/build.sh
@@ -2,17 +2,21 @@ mkdir ./_build/tmp/basic -p
 mkdir ./_build/tmp/full -p
 
 echo "Building standard hosts . . ."
-find ./_data/basic -type f -name "*.txt" | xargs cat > ./_build/tmp/basic/all.txt
-sort ./_build/tmp/basic/all.txt -u > ./_build/tmp/basic/standby.txt
-cat ./_build/head.txt ./_build/tmp/basic/standby.txt > ./_build/tmp/basic/hosts
+find ./_data/basic -type f -name "*.txt" | xargs cat > ./_build/tmp/basic/1.txt
+sed -e '/^#/d' ./_build/tmp/basic/1.txt > ./_build/tmp/basic/2.txt
+sort ./_build/tmp/basic/2.txt -u > ./_build/tmp/basic/3.txt
+cat ./_build/head.txt ./_build/tmp/basic/3.txt > ./_build/tmp/basic/hosts
 echo "Cleaning tmp . . ."
-rm ./_build/tmp/basic/all.txt
-rm ./_build/tmp/basic/standby.txt
+rm ./_build/tmp/basic/1.txt
+rm ./_build/tmp/basic/2.txt
+rm ./_build/tmp/basic/3.txt
 
 echo "Building full hosts . . ."
-find ./_data -type f -name "*.txt" | xargs cat > ./_build/tmp/full/all.txt
-sort ./_build/tmp/full/all.txt -u > ./_build/tmp/full/standby.txt
-cat ./_build/head.txt ./_build/tmp/full/standby.txt > ./_build/tmp/full/hosts
+find ./_data -type f -name "*.txt" | xargs cat > ./_build/tmp/full/1.txt
+sed -e '/^#/d' ./_build/tmp/full/1.txt > ./_build/tmp/full/2.txt
+sort ./_build/tmp/full/2.txt -u > ./_build/tmp/full/3.txt
+cat ./_build/head.txt ./_build/tmp/full/3.txt > ./_build/tmp/full/hosts
 echo "Cleaning tmp . . ."
-rm ./_build/tmp/full/all.txt
-rm ./_build/tmp/full/standby.txt
+rm ./_build/tmp/full/1.txt
+rm ./_build/tmp/full/2.txt
+rm ./_build/tmp/full/3.txt
