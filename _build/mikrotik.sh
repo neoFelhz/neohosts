c186c3c53ac032079aae7dc07592cb2250522fcd
neoFelhz <neofelhz@gmail.com>
2018-05-15 00:02:43 +0800

    chore/fix: try to fix directories error

--- _build/mikrotik.sh
+++ _build/mikrotik.sh
@@ -1,5 +1,5 @@
 echo "Building Mikrotik Basic Shell . . ."
-cp ./_build/tmp/basic/hosts ./_build/tmp/basic/1.txt
+cat ./_build/tmp/basic/hosts > ./_build/tmp/basic/1.txt
 sed -e '/^:/d' ./_build/tmp/basic/1.txt > ./_build/tmp/basic/2.txt
 sed -i 's|0.0.0.0 |ip dns static add address=$1 name=|g' ./_build/tmp/basic/2.txt
 cat ./_build/tmp/basic/2.txt > ./_build/tmp/basic/mikrotik.sh
@@ -8,7 +8,7 @@ rm ./_build/tmp/basic/1.txt
 rm ./_build/tmp/basic/2.txt
 
 echo "Building Mikrotik Full Shell . . ."
-cp ./_build/tmp/full/hosts ./_build/tmp/full/1.txt
+cat ./_build/tmp/full/hosts > ./_build/tmp/full/1.txt
 sed -e '/^:/d' ./_build/tmp/full/1.txt > ./_build/tmp/full/2.txt
 sed -i 's|0.0.0.0 |ip dns static add address=$1 name=|g' ./_build/tmp/full/2.txt
 cat ./_build/tmp/full/2.txt > ./_build/tmp/full/mikrotik.sh
