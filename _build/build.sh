95f80db7078144fb6ddf11e0f41865a8c9648282
neoFelhz <neofelhz@gmail.com>
2018-05-15 12:39:48 +0800

    chore/fix: mikrotik build

--- _build/build.sh
+++ _build/build.sh
@@ -43,3 +43,21 @@ cat ./_build/tmp/2.txt > ./_build/tmp/127.0.0.1/full/hosts
 echo "Cleaning tmp . . ."
 rm ./_build/tmp/1.txt
 rm ./_build/tmp/2.txt
+
+echo "Building Mikrotik Basic Shell . . ."
+cat ./_build/tmp/basic/hosts > ./_build/tmp/basic/1.txt
+sed -e '/^:/d' ./_build/tmp/basic/1.txt > ./_build/tmp/basic/2.txt
+sed -i 's|0.0.0.0 |ip dns static add address=$1 name=|g' ./_build/tmp/basic/2.txt
+cat ./_build/tmp/basic/2.txt > ./_build/tmp/basic/mikrotik.sh
+echo "Cleaning tmp . . ."
+rm ./_build/tmp/basic/1.txt
+rm ./_build/tmp/basic/2.txt
+
+echo "Building Mikrotik Full Shell . . ."
+cat ./_build/tmp/full/hosts > ./_build/tmp/full/1.txt
+sed -e '/^:/d' ./_build/tmp/full/1.txt > ./_build/tmp/full/2.txt
+sed -i 's|0.0.0.0 |ip dns static add address=$1 name=|g' ./_build/tmp/full/2.txt
+cat ./_build/tmp/full/2.txt > ./_build/tmp/full/mikrotik.sh
+echo "Cleaning tmp . . ."
+rm ./_build/tmp/full/1.txt
+rm ./_build/tmp/full/2.txt
